69d0c3b66d21259ee2731ec87839d2e91aa0e24d
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2022-01-15 09:22:59 +0700

    Update pacar.py

--- pacar.py
+++ pacar.py
@@ -1,3 +1,17 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
 os.system("clear")
 os.system("figlet RONALDO SEPTI")
 print

